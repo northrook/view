b06b9002f06aa60a151301304823e7da37b331b5
Martin <hello@northrook.com>
2025-01-10 10:01:14 +0000

    mend

--- src/Compiler/RegisterComponentAssetsPass.php
+++ src/Compiler/RegisterComponentAssetsPass.php
@@ -23,14 +23,43 @@ final class RegisterComponentAssetsPass extends CompilerPass
         if ( $this->validateRequiredServices( $container ) ) {
             return;
         }
-
-        $this->registerComponentAssets();
+        // $this->registerComponentAssets();
     }
 
     protected function registerComponentAssets() : void
     {
-        dump( $this->container->findTaggedServiceIds( $this->locatorID ) );
+        foreach ( \array_keys( $this->container->findTaggedServiceIds( $this->locatorID ) ) as $serviceId ) {
+            $componentDefinition = $this->container->getDefinition( $serviceId );
+        }
     }
+
+    // protected function registerComponentAssets() : void
+    // {
+    //     foreach ( \array_keys( $this->container->findTaggedServiceIds( $this->locatorID ) ) as $serviceId ) {
+    //         $componentDefinition = $this->container->getDefinition( $serviceId );
+    //
+    //         $classInfo = new ClassInfo( $componentDefinition->getClass() );
+    //         $fileInfo  = $classInfo->fileInfo;
+    //         $dirInfo   = new FileInfo( $fileInfo->getPath() );
+    //
+    //         $componentAssetFiles = $dirInfo->glob( [ '/*.css', '/*.js' ], asFileInfo : true );
+    //
+    //         foreach ( $componentAssetFiles as $assetFile ) {
+    //             // $assetReference = $this->assetManifest->getReference( $assetFile );
+    //
+    //             // $type = Type::from( $assetFile->getExtension() );
+    //             // $path = $assetFile->getPathname();
+    //
+    //             dump(
+    //                     AssetReference::generateName( $assetFile ),
+    //                     AssetReference::generateName( 'C:\laragon\www\symfony-framework\assets\styles\core\color.intent.css' ),
+    //             // $type,
+    //             // $path,
+    //             // $assetReference,
+    //             );
+    //         }
+    //     }
+    // }
 
     private function validateRequiredServices( ContainerBuilder $container ) : bool
     {
@@ -43,7 +72,7 @@ final class RegisterComponentAssetsPass extends CompilerPass
 
         $manifestArguments = $container->getDefinition( $this->manifestID )->getArguments();
 
-        if ( ! empty( $manifestArguments ) ) {
+        if ( empty( $manifestArguments ) ) {
             $message = [
                 $this::class." cannot find required '{$this->manifestID}' arguments.",
                 "\nEnsure this CompilerPass is TYPE_OPTIMIZE or later.",
@@ -52,7 +81,11 @@ final class RegisterComponentAssetsPass extends CompilerPass
             return true;
         }
 
-        $this->assetManifest = new AssetManifest( ...$manifestArguments );
+        $assetManifestPath = $manifestArguments[0] ?? null;
+
+        \assert( \is_string( $assetManifestPath ) );
+
+        $this->assetManifest = new AssetManifest( $assetManifestPath );
 
         return false;
     }
