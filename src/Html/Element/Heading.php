75119bf25b858c431b998bc27afb0ca3e79127dc
Martin <hello@northrook.com>
2025-01-11 11:11:09 +0000

    mend

--- src/Html/Element/Heading.php
+++ src/Html/Element/Heading.php
@@ -21,4 +21,39 @@ final class Heading extends Element
     ) {
         parent::__construct( 'h'.(string) $level, $attributes, ...$content );
     }
+
+    /**
+     * Expects a valid level string or int.
+     *
+     * - `h1-h6`
+     * - `1-6`
+     *
+     * Uses {@see assert} to validate before returning.
+     *
+     * @param int|string $level
+     *
+     * @return int<1, 6>
+     */
+    public static function validLevel( string|int $level ) : int
+    {
+        if ( \is_string( $level ) ) {
+            \assert(
+                ( \strlen( $level ) === 1 || \strlen( $level ) === 2 ),
+                "Heading levels must be one of h1, h2, etc. or 1, 2, etc; '{$level}' provided.",
+            );
+
+            \assert(
+                ( \strlen( $level ) === 2 && \strtolower( $level[0] ) === 'h' ),
+                "Heading levels must be one of h1, h2, etc; '{$level}' provided.",
+            );
+
+            $level = (int) \ltrim( $level, 'h' );
+        }
+
+        \assert(
+            \in_array( $level, [1, 2, 3, 4, 5, 6], true ),
+            "Heading level must be between 1 and 6. '{$level}' provided.",
+        );
+        return $level;
+    }
 }
